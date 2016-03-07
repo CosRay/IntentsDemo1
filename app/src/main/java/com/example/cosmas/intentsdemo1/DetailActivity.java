e023d3cc0ca64900a2b325ae8417f004bf875959
CosRay <cosmaskiruki@yahoo.com>
2016-03-08 00:20:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cosmas/intentsdemo1/DetailActivity.java
@@ -0,0 +1,17 @@
+package com.example.cosmas.intentsdemo1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by cosmas on 3/7/2016.
+ */
+public class DetailActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+     {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_layout);
+   }
+}
